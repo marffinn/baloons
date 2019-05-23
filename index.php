23c6dea36ab0976d3b9ddb5b6bf54e82a6fd93d6
Tomasz Drozdowski <marffinn@gmail.com>
2019-05-23 12:05:46 +0200

    commi

--- index.php
+++ index.php
@@ -1,14 +1,90 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width = device-width, initial-scale = 1.0">
-        <meta http-equiv="X-UA-Compatible" content = "ie=edge">
-        <title> Konfigurator </title>
-    </head>
-    
-    <body>
-        
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width = device-width, initial-scale = 1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet">
+    <link href="css/bulma.min.css" rel="stylesheet">
+    <title> Konfigurator </title>
+</head>
+
+<body>
+
+
+
+    <div class="container">
+        <div class="notification">
+
+            <p class="level-item has-text-centered">
+                <img src="assets/logo.svg" alt="Logotype" style="height: 45px; margin:10px">
+            </p>
+            <p class="level-item has-text-centered">
+                Witaj w konfiguratorze balonów
+            </p>
+
+        </div>
+
+        <div class="notification">
+
+            <p class="level-item has-text-centered">
+                Wybierz produkt do konfiguracji
+            </p>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <nav class="level">
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Tweets</p>
+                    <p class="title">3,456</p>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Following</p>
+                    <p class="title">123</p>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Followers</p>
+                    <p class="title">456K</p>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Likes</p>
+                    <p class="title">789</p>
+                </div>
+            </div>
+        </nav>
+
+        <div class="columns">
+            <div class="column">
+                First column
+            </div>
+            <div class="column">
+                Second column
+            </div>
+            <div class="column">
+                Third column
+            </div>
+            <div class="column">
+                Fourth column
+            </div>
+        </div>
+
+    </div>
+
+
+
     <script src="lib/three.js" type="text/javascript"></script>
-    </body>
+</body>
+
 </html>
