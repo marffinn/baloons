6094420d6f25f5fe721040cfc70189d194c0e528
Tomasz Drozdowski <marffinn@gmail.com>
2019-05-27 12:41:11 +0200

    background tweaks

--- index_2.php
+++ index_2.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width = device-width, initial-scale = 1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="assets/favicon.ico" />
+    <link rel="shortcut icon" href="favicon.ico" />
     <link href="style.css" rel="stylesheet">
     <link href="css/bulma.min.css" rel="stylesheet">
     <title> Konfigurator </title>
