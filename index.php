99f58ca1f19ce317645340a1090cfd538f90c261
Tomasz Drozdowski <marffinn@gmail.com>
2019-05-28 07:33:28 +0200

    :+1:

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width = device-width, initial-scale = 1.0">
@@ -12,31 +11,20 @@
 
 <body>
 
-
     <div class="container">
         <div class="notification">
-
             <p class="level-item has-text-centered">
-                <img src="assets/img/logo.svg" alt="Logotype" class="logotype" style="height: 45px">
+              <img src="assets/img/logo.svg" alt="Logotype" class="logotype" style="height: 45px">
             </p>
-            <p class="level-item has-text-centered">
-                Witaj w konfiguratorze balonów
-            </p>
-
+            <p class="level-item has-text-centered"> Witaj w konfiguratorze balonów </p>
         </div>
-
         <div class="notification" style="margin-bottom:10px">
-
-            <p class="level-item has-text-centered">
-                Wybierz produkt do konfiguracji:
-            </p>
-
+            <p class="level-item has-text-centered"> Wybierz produkt do konfiguracji: </p>
         </div>
     </div>
 
     <div class="container">
         <div class="columns is-multiline is-mobile is-centered" style="padding:20 0">
-
             <div class="column is-one-quarter">
                 <img src="assets/BALONY/Kula/Kula_4m_.jpg" />
             </div>
@@ -49,9 +37,6 @@
             <div class="column is-one-quarter">
                 <img src="assets/BALONY/Kula/Kula_4m_.jpg" />
             </div>
-
-            <!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
-
             <div class="column is-one-quarter">
                 <img src="assets/BALONY/Kula/Kula_4m_.jpg" />
             </div>
@@ -64,9 +49,9 @@
             <div class="column is-one-quarter">
                 <img src="assets/BALONY/Kula/Kula_4m_.jpg" />
             </div>
-
         </div>
     </div>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.13.1/TweenMax.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
     <script src="lib/index.js" type="text/javascript"></script>
