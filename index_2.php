77c4ab84f455122fe0c0c951cb5db1d0fbd179d7
Tomasz Drozdowski <marffinn@gmail.com>
2019-05-31 12:26:32 +0200

    ..

--- index_2.php
+++ index_2.php
@@ -1,6 +1,4 @@
-<?php
-    $numer = 0;
-?>
+<?php $numer = 0; ?>
 <!DOCTYPE html>
 <html>
 
@@ -17,8 +15,13 @@
 <body>
 
     <div id="container"></div>
+    <div id="picklist">
 
+        <div class="select skolory"></div>
+        <div class="select srozmiar"></div>
+        <div class="select sdodatki"></div>
 
+    </div>
 
     <div class="button_container">
         <section>
@@ -34,11 +37,11 @@
     </div>
 
     <!-- scripts & libraries -->
-    <script src="lib/WebGL.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/104/three.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tween.js/17.3.5/Tween.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+    <script src="lib/WebGL.js" type="text/javascript"></script>
     <script src="lib/threex.fullscreen.js" type="text/javascript"></script>
     <script src="lib/loaders/JSONLoader.js" type="text/javascript"></script>
     <script src="lib/orbitcontrols.js" type="text/javascript"></script>
